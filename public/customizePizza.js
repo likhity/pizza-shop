b815cddfef7c17020192e13e882a602b459933b8
Joaquin Uribe <joaquin.uribe.acosta@gmail.com>
2022-11-09 19:39:10 -0700

    PS-25 Finished customizePizzaPage

--- public/customizePizza.js
+++ public/customizePizza.js
@@ -39,8 +39,6 @@ class PizzaDisplay {
     }
   }
 
-
-
   //pizzaType set
   setPizzaType(pizzaType) {
     this.pizzaType = pizzaType //pizzaType is string
@@ -48,21 +46,56 @@ class PizzaDisplay {
     const pizzaTypeImage = document.querySelector("img[alt='pizzaType']");
     pizzaTypeImage.src = `/pizzaType/${this.pizzaType}.png`;
   }
+
+  getPizzaType() {
+    return this.pizzaType;
+  }
+
+  getToppings() {
+    const toppings = [];
+    if (this.mushroom) toppings.push("mushroom")
+    if (this.onions) toppings.push("onions")
+    if (this.olives) toppings.push("olives")
+    if (this.extraCheese) toppings.push("extraCheese")
+
+    return toppings;
+  }
 }
+//end of class
 
 const customizePizzaForm = document.getElementById("customizePizzaForm");
+const nextBtn = document.getElementById("nextBtn");
 
 const PizzaViewer = new PizzaDisplay();
 
 customizePizzaForm.addEventListener("input", (e) => {
-
+  
   //sets each topping to T/F based on if its checked in
   const mushroom = customizePizzaForm.mushroom.checked
   const extraCheese = customizePizzaForm.extraCheese.checked
   const olive = customizePizzaForm.olive.checked
   const onions = customizePizzaForm.onions.checked
-
+  
   PizzaViewer.setToppings(mushroom, extraCheese, olive, onions);
   console.log(customizePizzaForm.querySelector("input[type='radio']:checked").value);
   PizzaViewer.setPizzaType(customizePizzaForm.querySelector("input[type='radio']:checked").value);
+})
+
+//new class defines entire pizzaOrder
+const order = { 
+  pickUpTime: new Date(),
+  pizzaType: "Cheese",
+  toppings: [],
+  specialInstructions: "",
+ };
+
+ 
+//next Button Event Listener
+nextBtn.addEventListener("click", (e) => {
+  order.pizzaType = PizzaViewer.getPizzaType();
+  order.toppings = PizzaViewer.getToppings();
+  
+  localStorage.setItem("SUN_DEVIL_PIZZA_ORDER", JSON.stringify(order));
+
+  window.location.assign("/student/pickup-time");
 })
