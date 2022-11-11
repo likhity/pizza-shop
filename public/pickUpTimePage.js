48e38d4d307315e1e978fa4c3ed032cbaba35d76
Joaquin Uribe <joaquin.uribe.acosta@gmail.com>
2022-11-11 16:01:58 -0700

    PS-27 finished specialInstructionPage, reworked customizePizzaPage/pickUpTimePage to show session information when returning

--- public/pickUpTimePage.js
+++ public/pickUpTimePage.js
@@ -1,29 +1,56 @@
-
-const nxtBtn = document.getElementById("nextBtnpickTime");
 const hourTimeSelect = document.getElementById("hourTime");
 const minuteTimeSelect = document.getElementById("minuteTime");
 const amPmSelect = document.getElementById("amPm");
+const nxtBtn = document.getElementById("nextBtnpickTime");
 
-// vars to store value of user choice times
 const STORAGE_KEY = "SUN_DEVIL_PIZZA_ORDER";
 
+//retrieve sessionStroage (to use time to display)
+//NOTE: there will be a sessionStorage since we came from customizePizzaPage
+//which set a sessionStorage
+const savedOrder = JSON.parse(sessionStorage.getItem(STORAGE_KEY));
+
+var hourTime = "";
+var minuteTime = "";
+var amPm = "";
+
+//if there is a pickUpTime in sessionStorage
+if (savedOrder.pickUpTime){
+
+    //retrieve pickUpTime
+    const parsedPickUpTimeArray = savedOrder.pickUpTime.split(":");
+
+    // pickUpTime format = "01h:15m:am"
+    //set corresponding values 
+    hourTime = parsedPickUpTimeArray[0];
+    minuteTime = parsedPickUpTimeArray[1];
+    amPm = parsedPickUpTimeArray[2];
+
+    //find values hour = "01h", minute = "15m", amPm = "am"
+    //select form value = value retrieved from sessionStorage
+    hourTimeSelect.value = hourTime;
+    minuteTimeSelect.value = minuteTime;
+    amPmSelect.value = amPm;    
+}
 
 nxtBtn.addEventListener("click", (e) => {
     
+    //retrieve sessionStorage of order
     const order = JSON.parse(sessionStorage.getItem(STORAGE_KEY));
-    //get user input
-    console.log(order);
-    const hourTime = hourTimeSelect.value;
-    const minuteTime = minuteTimeSelect.value;
-    const amPm = amPmSelect.value;
-    console.log(hourTime)
-    console.log(minuteTime)
-    console.log(amPm)
 
-    order.pickUpTime = `${hourTime}:${minuteTime} ${amPm}`;
-    
+    //get user input
+    hourTime = hourTimeSelect.value;
+    minuteTime = minuteTimeSelect.value;
+    amPm = amPmSelect.value;
+
+    //set retrieved sessionStorage order's pickUpTime to userInput pickupTime
+    //NOTE: used " : " for parsing when retrieving pickUpTime
+    //format "05h:35m:am"
+    order.pickUpTime = `${hourTime}:${minuteTime}:${amPm}`;
+
+    //set sessionStorage to newOrder w/ new pickUpTime
+    //send user to next page (specialInstructions)
     sessionStorage.setItem(STORAGE_KEY, JSON.stringify(order));
-    
     window.location.assign("/student/special-instructions");
 })
 
---
+++ public/specialInstructions.js
@@ -0,0 +1,31 @@
+const textArea = document.getElementById("specialInstructions");
+const nxtBtn = document.getElementById("nextBtn");
+const STORAGE_KEY = "SUN_DEVIL_PIZZA_ORDER";
+
+
+//get order from user session storage
+//and set textArea to sessionStorage specialInstructions
+const order = JSON.parse(sessionStorage.getItem(STORAGE_KEY))
+textArea.value = order.specialInstructions;
+
+
+//----------------------------------------------------------------------------------------------------
+nxtBtn.addEventListener("click", (e) => {
+
+
+    //get userinput from the textarea
+    //show in console
+    //set order's special instructions (from sessionStorage)
+    //reStoreSessionStorage w/ new specialInstructions
+    userInputTextArea = textArea.value;
+    console.log(userInputTextArea);
+    order.specialInstructions = userInputTextArea;
+    sessionStorage.setItem(STORAGE_KEY, JSON.stringify(order));
+
+
+    //route is based off server.js > studentRoutes > studentControllers
+    window.location.assign("/student/order-summary");
+})
+//----------------------------------------------------------------------------------------------------
+
+
