971f057516a4c189c76f680687929427ecaf357b
Riley Than <rileythan01@gmail.com>
2022-11-10 19:06:37 -0700

    PS-31 Completed Chef Order Get Induvidual

--- controllers/chefController.js
+++ controllers/chefController.js
@@ -6,7 +6,9 @@ const ChefUser = require("../models/ChefUser");
 module.exports.order_list_get = async (req, res) => {
   res.render("chef/chefOrderListPage");
 };
-module.exports.order_get = async (req, res) => {};
+module.exports.order_get = async (req, res) => {
+  res.render("chef/chefSpecificOrder");
+};
 
 module.exports.confirm_cooking_post = async (req, res) => {};
 module.exports.confirm_ready_to_pickup_post = async (req, res) => {};
