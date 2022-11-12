ee88b4dd32c11ffead0d13be233dbf9850334cbb
Jerry <Jerry.lyx20020330@gmail.com>
2022-11-11 20:47:07 -0700

    PS-32 Finished confrom ready to cook

--- routes/chefRoutes.js
+++ routes/chefRoutes.js
@@ -13,7 +13,7 @@ router.use(requireChefAuth);
 router.get("/order-list", chefController.order_list_get);
 router.get("/order/:orderID", chefController.order_get);
 
-// router.post("/confirm-cooking/:orderID", chefController.confirm_cooking_post);
+router.post("/confirm-cooking/:orderID", chefController.confirm_cooking_post);
 router.post("/confirm-ready-to-pickup/:orderID", chefController.confirm_ready_to_pickup_post);
  router.get("/order-status/:orderID", chefController.order_status_get);
 
