d454a6c75283bca303aaca94999132fe8f6570c0
Jerry <Jerry.lyx20020330@gmail.com>
2022-11-11 19:51:42 -0700

    PS-33 Finished confrom ready to pickup post

--- controllers/chefController.js
+++ controllers/chefController.js
@@ -11,7 +11,20 @@ module.exports.order_get = async (req, res) => {
 };
 
 module.exports.confirm_cooking_post = async (req, res) => {};
-module.exports.confirm_ready_to_pickup_post = async (req, res) => {};
+module.exports.confirm_ready_to_pickup_post = async (req, res) => {
+  try {
+    const { orderID } = req.params;
+
+    const order = await AcceptedOrder.findOne({ orderID });
+
+    await AcceptedOrder.updateOne({ orderID }, { orderStatus: "Ready to Pickup" });
+
+    res.status(200).json({ success: true });
+
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
+};
 module.exports.order_status_get = async (req, res) => {
   try {
     const { orderID } = req.body;
